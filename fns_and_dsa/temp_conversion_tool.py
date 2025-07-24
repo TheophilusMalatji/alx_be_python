b1eaad75bbe5fa3afe40158f20e838e4c12bebc6
Theophilus Malatji <theo.malatji@gmail,com>
2025-07-25 00:10:59 +0200

    Week 5 task 4 exception handling

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,3 +1,5 @@
+from unicodedata import digit
+
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
@@ -12,10 +14,17 @@ def convert_to_fahrenheit(celsius):
     fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
     print(f"{celsius}°C is {fahrenheit}°F")
 
-temperature = float(input("Enter the temperature to convert:"))
-C_F = input("Is this temperature in Celsius or Fahrenheit? (C/F):").upper()
 
-if C_F == "C":
-    convert_to_fahrenheit(temperature)
-elif C_F == "F":
-    convert_to_celsius(temperature)
+try:
+    temperature = float(input("Enter the temperature to convert: "))
+except ValueError:
+    print("Invalid temperature. Please enter a numeric value.")
+else:
+    C_F = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
+
+    if C_F == "C":
+        convert_to_fahrenheit(temperature)
+    elif C_F == "F":
+        convert_to_celsius(temperature)
+    else:
+        print("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
