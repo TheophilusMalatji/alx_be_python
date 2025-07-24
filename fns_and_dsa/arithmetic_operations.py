d676df8af1314ceefcc242ffd4b595781f6bf127
Theophilus Malatji <theo.malatji@gmail,com>
2025-07-24 22:48:03 +0200

    week 5 task 1

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,7 +7,10 @@ def perform_operation(num1,num2,operation):
     elif operation == "multiply":
         return num1 * num2
     if operation == "divide":
-        return num1 / num2
+        if num2 == 0:
+            print("Cannot divide by zero")
+        else:
+            return num1 / num2
 
 
 def main():
